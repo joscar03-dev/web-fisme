9324ebda585bfa37897523b20ca73cb6ede62c28
josueCayotopa <7583976221@untrm.edu.pe>
2024-10-06 00:08:39 -0500

    Tablas slug concursos

--- app/Models/Consurso.php
+++ app/Models/Consurso.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Consurso extends Model
 {
     use HasFactory;
+    protected $table = 'consursos';
+
+    // Define los campos que pueden ser asignados en masa
+    protected $fillable = [
+        'nombre',
+        'slug',
+        'tipo_concurso',
+        'fecha_inicio',
+        'hora',
+        'nombres_completos',
+        'fecha_fin',
+        'estado',
+    ];
+
 }
