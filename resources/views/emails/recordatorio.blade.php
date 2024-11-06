e8f31d71237d1a7d6a1f5a039a38d0243615d87f
joscar03-dev <locomancocapac@gmail.com>
2024-11-06 17:21:16 -0500

    creacion de mail para recordatorio

---
+++ resources/views/emails/recordatorio.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Gracias por tu inscripción</title>
+</head>
+<body>
+    <h1>{{ $data['titulo'] }}</h1>
+    <p>{{ $data['mensaje'] }}</p>
+</body>
+</html>
